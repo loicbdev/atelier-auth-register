e88d3e5a69c473a954ca5fb589092dd46a0ff3e2
loicbdev <loicbodolec@msn.com>
2021-01-19 22:30:18 +0100

    update CSS

--- front/src/components/styleLogin.js
+++ front/src/components/styleLogin.js
@@ -5,18 +5,13 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     display: "flex",
     flexDirection: "column",
-    alignItems: "center",
+    alignItems: "left",
   },
   someInput: {
-    margin: "5px 0",
+    margin: "2px 0",
     textAlign: "left",
     backgroundColor: theme.palette.common.white,
     paddingBottom: "1.5rem",
-  },
-  list: {
-    margin: "7px 0",
-    textAlign: "center",
-    fontSize: "1.2rem",
   },
 }));
 
--- front/src/components/styleRegister.js
+++ front/src/components/styleRegister.js
@@ -8,15 +8,10 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "left",
   },
   someInput: {
-    margin: "5px 0",
+    margin: "2px 0",
     textAlign: "left",
     backgroundColor: theme.palette.common.white,
     paddingBottom: "1.5rem",
-  },
-  list: {
-    margin: "7px 0",
-    textAlign: "center",
-    fontSize: "1.2rem",
   },
 }));
 
